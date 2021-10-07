1bf799380a65ffebc56f41259984ae21753e3057
Rodrigo Aquino <63016845+rodrigoAquino123@users.noreply.github.com>
2021-10-07 20:11:02 -0300

    Add files via upload

--- questao02.py
+++ questao02.py
@@ -1,44 +1,79 @@
 import sqlite3
+import time
 
-conector = sqlite3.connect("agenda.db")
-cursor = conector.cursor()
-print(''' Insira o número  da opçao desejada:
-    1 - listar
-    2 - cadastrar
-    3 - alterar
-    4 - excluir
-    5 - sair
-    ''')
-Ler = int(input())
+ModoExecutar = True
 
-if Ler == 1:
-    sql = "select *from contatos"
-    cursor.execute(sql)
-    dados = cursor.fetchall()
+while ModoExecutar:
+    conector = sqlite3.connect("agenda.db")
+    cursor = conector.cursor()
+    time.sleep(2)
+    print(''' Insira o número  da opçao desejada:
+        1 - listar
+        2 - cadastrar
+        3 - alterar
+        4 - excluir
+        5 - sair
+        ''')
+    Ler = int(input())
 
-    print("-"*35)
-    print("{:<12} {:25} {:12} {:10} {:20} {:>6}".format("NumContato","Nome","Cel","Tel","Email","Aniver"))
-    print("-"*120)
-    for d in dados:
-        print("{:<12} {:25} {:12} {:10} {:20} {:>6}".format(d[0],d[1],d[2],d[3],d[4],d[5]))
-    print("-"*120)
+    if Ler == 1:
+        sql = "select *from contatos"
+        cursor.execute(sql)
+        dados = cursor.fetchall()
 
-    print("Encontrados {} registros".format(len(dados)))
-    conector.close()
-if Ler == 2:
-    Nome = input('Nome: ')
-    Cel = input('Celular: ')
-    Tel = input('Telefone: ')
-    Email = input('Email: ')
-    Aniver = input('Aniversário: ')
+        print("-"*35)
+        print("{:<12} {:25} {:12} {:10} {:20} {:>6}".format("NumContato","Nome","Cel","Tel","Email","Aniver"))
+        print("-"*120)
+        for d in dados:
+            print("{:<12} {:25} {:12} {:10} {:20} {:>6}".format(d[0],d[1],d[2],d[3],d[4],d[5]))
+        print("-"*120)
 
-    sql = "insert into contatos(Nome,Cel,Tel,Email,Aniver) values (?,?,?,?,?)"
+        print("Encontrados {} registros".format(len(dados)))
+        conector.close()
+    if Ler == 2:
+        Nome = input('Nome: ')
+        Cel = input('Celular: ')
+        Tel = input('Telefone: ')
+        Email = input('Email: ')
+        Aniver = input('Aniversário: ')
 
-    cursor.execute(sql, (Nome, Cel, Tel, Email, Aniver))
-    conector.commit()
-    conector.close()
+        sql = "insert into contatos(Nome,Cel,Tel,Email,Aniver) values (?,?,?,?,?)"
 
-    print(f'{Nome} cadastrado com sucesso.')
+        cursor.execute(sql, (Nome, Cel, Tel, Email, Aniver))
+        conector.commit()
+        conector.close()
+
+        print(f'{Nome} cadastrado com sucesso.')
+    if Ler == 3:
+        NumContato = input('NumContato: ')
+        Cel = input('Celular: ')
+        Tel = input('Telefone: ')
+        Email = input('Email: ')
+
+        sql = "update contatos set Cel= ?, Tel = ?,Email = ? where NumContato = ?"
+
+        cursor.execute(sql, (Cel, Tel, Email, NumContato))
+        conector.commit()
+        conector.close()
+
+        print(f'{NumContato} dados alterados com sucesso.')
+    if Ler == 4:
+        NumContato = input('NumContato: ')
+
+        sql = "delete from contatos where NumContato = ?"
+
+        cursor.execute(sql, (NumContato,))
+        conector.commit()
+        conector.close()
+
+        print(f'NunContato {NumContato} excluído com sucesso.')
+    if Ler == 5:
+        exit()
+
+
+
+
+
 
 
 
